b4dfef9c8e0130813165e6278216e4d0a2a04553
Dani Solà <dani@dani-sola.com>
2015-08-16 09:36:28 +0100

    Store the user email in a cookie, so less typing is required

--- forward-cat-website/app/assets/javascripts/main.js
+++ forward-cat-website/app/assets/javascripts/main.js
@@ -30,6 +30,8 @@ $(document).ready(function () {
         url: "/add",
         type: "get",
         success: function() {
+          var userEmail = $('#proxy-form').find('#email').val();
+          createCookie("user_email", userEmail, 365);
           window.location.href = "/email_sent";
         }
       });
@@ -93,6 +95,11 @@ $(document).ready(function () {
   $('body').click(function() {
     hideTooltip(getElemsWithTooltip());  // Hide all tooltips
   });
+
+  var userEmail = readCookie("user_email");
+  if (userEmail !== null) {
+    $('#proxy-form').find('#email').val(userEmail);
+  }
 });
 
 function getElemsWithTooltip() {
@@ -111,4 +118,25 @@ function hideTooltip(elems) {
 function showTooltip(elems) {
   elems.popover('show');
   elems.addClass('tooltip-open');
+}
+
+function createCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toGMTString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
